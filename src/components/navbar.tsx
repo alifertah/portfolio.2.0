7c08c3e5f83cc49a07a9e01e094caa225abe1343
alifertah <alfertah@student.1337.ma>
2023-04-01 06:26:57 +0000

    fixing the error of updating the active tab

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,24 +3,27 @@ import Link from "next/link"
 import { useState } from "react"
 
 const navs = [
-    {name: "WHO AM I?", link: "/"},
-    {name: "SKILLS", link: "/"},
-    {name: "PROJECTS", link: "/"},
+    {name: "WHO AM I?", link: "/", id:1},
+    {name: "SKILLS", link: "/", id:2},
+    {name: "PROJECTS", link: "/", id:3},
 ]
 
-// function alertMsg(msg : any){
-
-// }
 
 export default function Navbar(){
-    const [tab, setTab] = useState("0")
+    const [activeTab, setActiveTab] = useState()
+
+    function activeId(activeTab : any){
+        setActiveTab(activeTab);
+        console.log(activeTab);
+    }
+
     return(
         <div  className="text-white sticky top-0 z-10 backdrop-filter backdrop-blur-md flex justify-between items-center py-4 px-3">
             <h1 className="text-xl font-bold animate-bounce">Ali Fertah</h1>
             <div className="flex justify-between w-[20%]">
             {
-                navs.map((nav) => (
-                        <Link  className="hover:text-sky-300 hover:scale-110 duration-300" href={nav.link}>{nav.name}</Link>
+                navs && navs.map((nav, id) => (
+                        <Link key={nav.id} onClick={()=> activeId(nav.name)}  className="hover:text-sky-300 hover:scale-110 duration-300" href={nav.link}>{nav.name}</Link>
                 ))
             }
             </div>
