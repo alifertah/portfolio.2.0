064bd7189754764f95324bf15b01581533267e7a
alifertah <alfertah@student.1337.ma>
2023-04-13 10:38:02 +0000

    handling clicked spans skills compo

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,19 +1,39 @@
+import { useState } from "react"
+
 const heads = ["Langs", "Frontend", "Tools"]
-const activeBg = "bg-gradient-to-r from-[#e71d36] to-[#011627]"
-
-
+const activeSec = "bg-gradient-to-r from-[#e71d36] to-[#011627] cursor-pointer px-8 rounded-2xl py-1"
+const notActiveSec = "cursor-pointer px-8 rounded-2xl py-1"
 
 export default function Skills(){
+
+    const [langsClass, setLangsClass] = useState(activeSec);
+    const [frontendClass, setFrontendClass] = useState(notActiveSec);
+    const [toolsClass, setToolsClass] = useState(notActiveSec);
+    
+    function handleLangsClick() {
+        setLangsClass(activeSec);
+        setFrontendClass(notActiveSec);
+        setToolsClass(notActiveSec);
+      }
+    
+      function handleFrontendClick() {
+        setLangsClass(notActiveSec);
+        setFrontendClass(activeSec);
+        setToolsClass(notActiveSec);
+      }
+    
+      function handleToolsClick() {
+        setLangsClass(notActiveSec);
+        setFrontendClass(notActiveSec);
+        setToolsClass(activeSec);
+      }
+
     return(
         <div className="flex justify-center w-[30%]">
             <div className="flex justify-between w-full">
-                {
-                    heads.map(h =>(
-                        <>
-                            <span className="cursor-pointer px-8 rounded-2xl py-1"> {h} </span>
-                        </>
-                        ))
-                }
+                 <span className={langsClass} onClick={handleLangsClick}> Langs </span>
+                 <span className={frontendClass} onClick={handleFrontendClick}> Frontend </span>
+                 <span className={toolsClass} onClick={handleToolsClick}> Tools </span>
             </div>
         </div>
     )
