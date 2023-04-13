af6e783cdf5b63f4fcf83c55eb22cac3bb77a332
alifertah <alfertah@student.1337.ma>
2023-04-13 10:43:43 +0000

    handling clicked spans skills compo

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -6,34 +6,47 @@ const notActiveSec = "cursor-pointer px-8 rounded-2xl py-1"
 
 export default function Skills(){
 
+    const [activeTab, setActiveTab] = useState("Langs")
     const [langsClass, setLangsClass] = useState(activeSec);
     const [frontendClass, setFrontendClass] = useState(notActiveSec);
     const [toolsClass, setToolsClass] = useState(notActiveSec);
     
     function handleLangsClick() {
+        setActiveTab("langs")
         setLangsClass(activeSec);
         setFrontendClass(notActiveSec);
         setToolsClass(notActiveSec);
       }
     
       function handleFrontendClick() {
+        setActiveTab("frontend")
         setLangsClass(notActiveSec);
         setFrontendClass(activeSec);
         setToolsClass(notActiveSec);
       }
     
       function handleToolsClick() {
+        setActiveTab("tools")
         setLangsClass(notActiveSec);
         setFrontendClass(notActiveSec);
         setToolsClass(activeSec);
       }
 
     return(
-        <div className="flex justify-center w-[30%]">
+        <div className="">
             <div className="flex justify-between w-full">
                  <span className={langsClass} onClick={handleLangsClick}> Langs </span>
                  <span className={frontendClass} onClick={handleFrontendClick}> Frontend </span>
                  <span className={toolsClass} onClick={handleToolsClick}> Tools </span>
+            </div>
+            <div>
+                {
+                    activeTab === "langs" ? ("langs") : (
+                            activeTab === "frontend" ? ("frontend") : (
+                                activeTab === "tools" ? ("tools") : ('')
+                            )
+                    )
+                }
             </div>
         </div>
     )
