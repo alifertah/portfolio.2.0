19c786a066017fc64599684c2f3d0f69ee40f81f
alifertah <alfertah@student.1337.ma>
2023-05-09 00:47:32 +0100

    adding all neccessary icons to skills section

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -2,11 +2,21 @@ import { useState } from "react"
 import Image from "next/image"
 
 const langs = [{name:"JAVASCRIPT", link:"/programmingIcons/javascript.svg"},
-{name:"JAVASCRIPT", link:"/programmingIcons/javascript.svg"}, 
-{name:"JAVASCRIPT", link:"/programmingIcons/javascript.svg"}, 
-{name:"JAVASCRIPT", link:"/programmingIcons/javascript.svg"}, ]
-const frontEnd = ["REACT.JS/NEXT.JS", "TAILWINDCSS", "FRAMEMOTION", "CHARTJS"]
-const tools = ["GIT", "VSCODE", "FIGMA", "GITHUB"]
+{name:"TYPESCRIPT", link:"/programmingIcons/typescript.svg"}, 
+{name:"C", link:"/programmingIcons/c.svg"}, 
+{name:"PYTHON", link:"/programmingIcons/python.svg"}, ]
+
+const frontEnd = [{name:"REACT.JS/NEXT.JS", link:"/programmingIcons/react.svg"},
+{name:"TAILWINDCSS", link:"/programmingIcons/tailwind.svg"},
+{name:"FRAMEMOTION",link:"/programmingIcons/framerMotion.svg"},
+{name:"CHARTJS", link:"/programmingIcons/chartJs.svg"}]
+
+const tools = [{name:"GIT", link:"/programmingIcons/git.svg"},
+{name:"GITHUB", link:"/programmingIcons/github.svg"},
+{name:"VSCODE", link:"/programmingIcons/vscode.svg"},
+{name:"FIGMA", link:"/programmingIcons/figma.svg"},
+]
+
 const activeSec = "bg-gradient-to-r from-[#e71d36] to-[#011627] cursor-pointer px-8 rounded-2xl py-1"
 const notActiveSec = "cursor-pointer px-8 rounded-2xl py-1"
 
@@ -48,18 +58,28 @@ export default function Skills(){
             <div className="flex flex-col justify-center  font-bold italic text-left mt-5">
                 {
                     activeTab === "langs" ? 
-                        langs.map(lang =>(<div className="flex justify-between items-center">
+                        langs.map(lang =>(<div className="flex justify-between items-center mt-2">
                             <div className="flex items-center">
                                 {lang.name}
                             </div>
-                            <Image src={lang.link} height="45" width="45" />
+                                <Image className="rounded" src={lang.link} height="40" width="40" />
                             </div>))
                     : (
                             activeTab === "frontend" ? (
-                                frontEnd.map(front=>(<div>{front.name}</div>))
+                                frontEnd.map(front=>(<div className="flex justify-between items-center mt-2">
+                                <div className="flex items-center">
+                                    {front.name}
+                                </div>
+                                    <Image className="rounded" src={front.link} height="40" width="40" />
+                                </div>))
                             ) : (
                                 activeTab === "tools" ? (
-                                    tools.map(tool=>(<div>{tool.name}</div>))
+                                    tools.map(tool=>((<div className="flex justify-between items-center mt-2">
+                                    <div className="flex items-center">
+                                        {tool.name}
+                                    </div>
+                                        <Image className="rounded" src={tool.link} height="40" width="40" />
+                                    </div>)))
                                 ) : ('')
                             )
                     )
