2b263737c6dcbc4bc1ec3821b0c7c4cdf41a19e2
alifertah <alifertah42@gmail.com>
2024-02-19 16:53:45 +0100

    feat : remove github api fetching

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -2,34 +2,37 @@ import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
 export default function Projects() {
-  const [repos, setRepos] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    async function fetchRepos() {
-      const response = await fetch('https://api.github.com/users/alifertah/repos');
-      const data = await response.json();
-      setRepos(data);
-    }
-    fetchRepos();
-  }, []);
+  const projects = [
+    {
+      name: "test",
+      link: "#",
+      description: "description"
+    },
+    {
+      name: "test1",
+      link: "#",
+      description: "description1"
+    },
+  ]
 
   const handleRightClick = () => {
-    setCurrentIndex(currentIndex + 1 >= repos.length ? 0 : currentIndex + 1);
+    setCurrentIndex(currentIndex + 1 >= projects.length ? 0 : currentIndex + 1);
   };
 
   const handleLeftClick = () => {
-    setCurrentIndex(currentIndex - 1 <= 0 ? repos.length -1 : currentIndex - 1);
+    setCurrentIndex(currentIndex - 1 <= 0 ? projects.length -1 : currentIndex - 1);
   };
 
   return (
     <div className='flex justify-between'>
-      <Image className='cursor-pointer' onClick={handleLeftClick} src="/angle-left.svg" width={40} height={40}/>
+      <Image alt="" className='cursor-pointer' onClick={handleLeftClick} src="/angle-left.svg" width={40} height={40}/>
       <div>
-        <h1>alifertah's Repositories:</h1>
-        <p>{repos[currentIndex]?.name}</p>
+        <h1>{projects[currentIndex].name}</h1>
+        <p>Project name</p>
       </div>
-      <Image className='cursor-pointer' onClick={handleRightClick} src="/angle-right.svg" width={40} height={40}/>
+      <Image alt="" className='cursor-pointer' onClick={handleRightClick} src="/angle-right.svg" width={40} height={40}/>
     </div>
   );
 }
