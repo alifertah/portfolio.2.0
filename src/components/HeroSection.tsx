7091f89b853999122292a7d427b2de5387243aa0
alifertah <alifertah42@gmail.com>
2024-11-18 18:11:26 +0000

    feat: noisy pics to break the black background color

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,11 +1,10 @@
-// components/sections/HeroSection.tsx
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
 
 const HeroSection = () => {
-
     const [dots, setDots] = useState("");
     const [showMessage, setShowMessage] = useState(false);
+
     useEffect(() => {
         // Interval to update dots
         const dotsInterval = setInterval(() => {
@@ -24,15 +23,16 @@ const HeroSection = () => {
             clearTimeout(messageTimeout);
         };
     }, []);
+
     return (
         <motion.div
-            className="h-screen flex items-center bg-gradient-hero text-grayLight"
+            className="h-screen relative flex items-center bg-gradient-hero text-grayLight"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 1 }}
         >
-            <div className="container mx-auto flex flex-col justify-between md:flex-row items-center px-6 md:px-12 space-y-10 md:space-y-0 md:space-x-12">
+            <div className="container mx-auto flex flex-col justify-between md:flex-row items-center px-6 md:px-12 space-y-10 md:space-y-0 md:space-x-12 z-10">
                 {/* Image Section */}
                 <motion.div
                     className="md:w-1/2 mb-8 md:mb-0"
@@ -55,27 +55,34 @@ const HeroSection = () => {
                     transition={{ duration: 1 }}
                 >
                     {!showMessage ? (
-                <h1 className="text-2xl font-bold">
-                    Initializing<span>{dots}</span>
-                </h1>
-            ) : (
-                <>
-                <h1 className="text-3xl font-bold ">
-                    🤖 Ali Fertah is now online. <br />
-                </h1>
-                    <p className="text-lg md:text-xl mb-6 text-gray-400">
-                        I am a passionate developer specializing in modern web technologies, creating engaging user experiences, and building impactful digital solutions.
-                    </p>
-                    <a
-                        href="#contact"
-                        className="px-6 py-3 bg-accent hover:bg-indigo-600 text-white font-semibold rounded-lg shadow-lg transition duration-300"
-                        >
-                        Contact Me
-                    </a>
+                        <h1 className="text-2xl font-bold">
+                            Initializing<span>{dots}</span>
+                        </h1>
+                    ) : (
+                        <>
+                            <h1 className="text-3xl font-bold">
+                                🤖 Ali Fertah is now online. <br />
+                            </h1>
+                            <p className="text-lg md:text-xl mb-6 text-gray-400">
+                                I am a passionate developer specializing in modern web technologies, creating engaging user experiences, and building impactful digital solutions.
+                            </p>
+                            <a
+                                href="#contact"
+                                className="px-6 py-3 bg-accent hover:bg-indigo-600 text-white font-semibold rounded-lg shadow-lg transition duration-300"
+                            >
+                                Contact Me
+                            </a>
                         </>
-            )}
+                    )}
                 </motion.div>
             </div>
+
+            {/* Positioned Background Image */}
+            <img
+                src="https://www.labrahmi.me/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fnoise.e4d8e2d8.webp&w=3840&q=75"
+                alt="Background Noise"
+                className="absolute bottom-0 left-0 w-full h-auto z-0"
+            />
         </motion.div>
     );
 };
